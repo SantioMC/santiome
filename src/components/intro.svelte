b4f5c1202056c7e4a718ab3f1bf7531b8cdf8ea1
SantioMC <admin@santio.me>
2024-11-03 21:19:44 -0500

    fix: hover effect on intro card

--- src/components/intro.svelte
+++ src/components/intro.svelte
@@ -9,3 +9,13 @@
 		Canada. I'm primarily a backend developer but in recent years I've been working with tools such as Kubernetes, Terraform, and other CI/CD tools to help manage and scale infrastructure for some of my work (For example, <Ref name="Hideaway" image="/img/hideaway.png" link="#hideaway" />).
 	</p>
 </div>
+
+<style>
+	.grow {
+		transition: all 0.2s ease-in-out;
+	}
+	
+	.grow:hover {
+		transform: scale(1.01);
+	}
+</style>
