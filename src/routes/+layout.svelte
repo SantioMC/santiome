8c9d7ee6a7bab14a82089a8109e1273a04d3269c
SantioMC <admin@santio.me>
2026-02-14 01:00:22 -0500

    fix: favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
 	import { Toaster } from 'svelte-sonner';
 	import './layout.css';
 
 	let { children } = $props();
 </script>
-
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
 <Toaster position="bottom-right" theme="dark" richColors />
 {@render children()}
