950efd51ecd5fe2954f64924a59f6707b122c609
SantioMC <admin@santio.me>
2026-04-10 02:57:54 -0400

    feat: ssh and uuid copy btns

---
+++ src/lib/components/copy-button.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { toast } from 'svelte-sonner';
+	import Icon from './icon.svelte';
+
+	const {
+		icon,
+		text,
+		copy
+	}: {
+		icon: string;
+		text: string;
+		copy: string;
+	} = $props();
+
+    const onclick = () => {
+        navigator.clipboard.writeText(copy);
+        toast.success('Copied to clipboard');
+    }
+</script>
+
+<button
+    {onclick}
+	class="card overshoot flex w-full cursor-pointer flex-row items-center gap-2 border-2 border-transparent text-sm transition-all select-none hover:scale-[1.02] hover:rotate-[0.5deg] hover:border-primary"
+>
+	<Icon {icon} />
+	{text}
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,67 +1,77 @@
 <script lang="ts">
 	import Badge from '$lib/components/badge.svelte';
+	import CopyButton from '$lib/components/copy-button.svelte';
 	import Project from '$lib/components/project.svelte';
 </script>
 
-<div class="card flex flex-col gap-8">
-	<div class="flex items-center gap-3">
-		<img src="/img/profile-picture.webp" alt="Avatar" class="h-12 w-12 rounded-full" />
+<div class="my-12 flex flex-col gap-4">
+	<div class="card flex flex-col gap-8">
+		<div class="flex items-center gap-3">
+			<img src="/img/profile-picture.webp" alt="Avatar" class="h-12 w-12 rounded-full" />
 
-		<div class="flex flex-col">
-			<h1 class="text-lg leading-6">Santio</h1>
-			<p class="font-mono text-sm leading-4 text-secondary">Software Engineer</p>
+			<div class="flex flex-col">
+				<h1 class="text-lg leading-6">Santio</h1>
+				<p class="font-mono text-sm leading-4 text-secondary">Software Engineer</p>
+			</div>
 		</div>
+
+		<section>
+			<p>
+				Hello there 👋, I'm Santio, a backend and infrastructure developer from Canada. I'm
+				primarily a backend developer but in recent years I've been working with tools such as
+				Kubernetes, Terraform, and other CI/CD tools to help manage and scale infrastructure for
+				some of my work. Check out my <a href="https://github.com/SantioMC" target="_blank"
+					>Github</a
+				>, or reach out to me on
+				<a href="https://discord.com/users/239752804468654090" target="_blank">Discord</a>
+			</p>
+		</section>
+
+		<section>
+			<h2 class="text-lg font-bold">Projects</h2>
+			<p>
+				I've worked on quite a few various projects, and a bit all over the place. See some of the
+				more prominent ones below, these are also places I've both contributed to or worked at.
+				Check them out 😋.
+			</p>
+
+			<div class="mt-4 flex flex-wrap gap-4">
+				<Project
+					name="GamerSafer"
+					src="/project/gamersafer.webp"
+					href="https://gamersafer.com/"
+					larger
+				/>
+				<Project name="play.hosting" src="/project/playhosting.svg" href="https://play.hosting/" />
+				<Project
+					name="Soul Studios"
+					src="/project/soulstudios.svg"
+					href="https://www.soulstudios.io/"
+				/>
+				<Project name="Lilypad Studios" src="/project/lilypad.svg" href="https://lilypad.gg/" />
+			</div>
+		</section>
+
+		<section>
+			<h2 class="text-lg font-bold">Friends</h2>
+			<p>
+				Check out some of these awesome people who are so extremely talented in what they do. Click
+				on their 88x31 badge to see their website.
+			</p>
+
+			<div class="mt-4 flex flex-wrap gap-2">
+				<Badge name="Santio" src="/badge/santio.webp" />
+				<Badge name="Aroze" src="/badge/aroze.png" href="https://aroze.me" />
+				<Badge name="Eva" src="/badge/eva.png" href="https://eva.ac/" />
+				<Badge name="Astrid" src="/badge/astrid.png" href="https://www.astrid.sh/" />
+				<Badge name="Lily" src="/badge/lily.webp" href="https://lily.pet/" />
+				<Badge name="Ollie" src="/badge/ollie.png" href="https://ollie.lol/" />
+			</div>
+		</section>
 	</div>
 
-	<section>
-		<p>
-			Hello there 👋, I'm Santio, a backend and infrastructure developer from Canada. I'm primarily
-			a backend developer but in recent years I've been working with tools such as Kubernetes,
-			Terraform, and other CI/CD tools to help manage and scale infrastructure for some of my work.
-			Check out my <a href="https://github.com/SantioMC" target="_blank">Github</a>, or reach out to
-			me on <a href="https://discord.com/users/239752804468654090" target="_blank">Discord</a>
-		</p>
-	</section>
-
-	<section>
-		<h2 class="text-lg font-bold">Projects</h2>
-		<p>
-			I've worked on quite a few various projects, and a bit all over the place. See some of the
-			more prominent ones below, these are also places I've both contributed to or worked at. Check
-			them out 😋.
-		</p>
-
-		<div class="mt-4 flex flex-wrap gap-4">
-			<Project
-				name="GamerSafer"
-				src="/project/gamersafer.webp"
-				href="https://gamersafer.com/"
-				larger
-			/>
-			<Project name="play.hosting" src="/project/playhosting.svg" href="https://play.hosting/" />
-			<Project
-				name="Soul Studios"
-				src="/project/soulstudios.svg"
-				href="https://www.soulstudios.io/"
-			/>
-			<Project name="Lilypad Studios" src="/project/lilypad.svg" href="https://lilypad.gg/" />
-		</div>
-	</section>
-
-	<section>
-		<h2 class="text-lg font-bold">Friends</h2>
-		<p>
-			Check out some of these awesome people who are so extremely talented in what they do. Click on
-			their 88x31 badge to see their website.
-		</p>
-
-		<div class="mt-4 flex flex-wrap gap-2">
-			<Badge name="Santio" src="/badge/santio.webp" />
-			<Badge name="Aroze" src="/badge/aroze.png" href="https://aroze.me" />
-			<Badge name="Eva" src="/badge/eva.png" href="https://eva.ac/" />
-			<Badge name="Astrid" src="/badge/astrid.png" href="https://www.astrid.sh/" />
-			<Badge name="Lily" src="/badge/lily.webp" href="https://lily.pet/" />
-			<Badge name="Ollie" src="/badge/ollie.png" href="https://ollie.lol/" />
-		</div>
-	</section>
+	<div class="flex items-center gap-4 mx-4 md:mx-auto lg:w-4xl flex-col md:flex-row md:w-auto">
+		<CopyButton icon='key' text='Copy SSH Key' copy='ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKV9lN6Map8pkOWPnCsZfdwe+7IHRt4sDOPXSdb/lCbD' />
+		<CopyButton icon='brand-minecraft' text='Copy Minecraft Unique ID' copy='0bfea1b6-8ed9-4eb9-93da-c4273bfa0a09' />
+	</div>
 </div>
